4a6a1cda2314811720f52183ce7b60e4bf78e540
KristianRykkje <kriryk@gmail.com>
2023-10-01 07:21:34 +0200

    Update some text

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -23,7 +23,7 @@ function Profile() {
         }}
       />
       <Typography variant="h4" gutterBottom color="#3f51b5">
-        Tilfeldig blomst
+        Tilfeldig fin blomst
       </Typography>
     </Stack>
   );
